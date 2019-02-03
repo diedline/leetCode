e0c662d7e9a87b1cd6880dfbca564e38ecadd717
tom_glo <goodmoring@qq.com>
2019-02-03 14:56:43 +0800

    第15题完成

--- src/com/diedline/leetcode/Test14.java
+++ src/com/diedline/leetcode/Test14.java
@@ -1,5 +1,12 @@
 package com.diedline.leetcode;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
+
 public class Test14 {
 }
 
@@ -18,13 +25,41 @@ public class Test14 {
  * 输出: ""
  * 解释: 输入不存在公共前缀。
  * 说明:
- *
+ *["aac","acab","aa","abba","aa"]
  * 所有输入只包含小写字母 a-z 。
  */
 
 class Solution14 {
+    @Test
+    public void test() {
+        String[] strs = {"ac", "ac", "a", "ac"};
+        System.out.println(longestCommonPrefix(strs));
+        System.out.println("------------------------");
+    }
+
+
     public String longestCommonPrefix(String[] strs) {
-        //做题做的我头疼 今天先到这里吧 学习框架去了
-        return "a";
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        StringBuilder sb = new StringBuilder();
+        if (strs.length > 1) {
+            int len = strs[0].length();
+            for (int i = 0; i < len; i++) {
+                char curr = strs[0].charAt(i);
+                //先取出第一个字符 然后用它跟后面的值比较
+                for (int j = 1; j < strs.length; j++) {
+                    //假如后面的字符串的长度比第一个字符串的长度小 或者是不匹配了就返回String aaa aa这种情况
+                    if (strs[j].length() <= i || strs[j].charAt(i) != curr) {
+                        return sb.toString();
+                    }
+                    //假如字符串的值和curr取出的值相同并且循环到最后都满足
+                    if (strs[j].charAt(i) == curr && j == strs.length - 1) {
+                        sb.append(curr);
+                    }
+                }
+            }
+        }
+        return sb.toString();
     }
 }
---
+++ src/com/diedline/leetcode/Test15.java
@@ -0,0 +1,100 @@
+package com.diedline.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Test15 {
+}
+
+/**
+ * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，
+ * 使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ *
+ * 满足要求的三元组集合为：
+ * [
+ *   [-1, 0, 1],
+ *   [-1, -1, 2]
+ * ]
+ */
+class Solution15 {
+
+    @Test
+    public void test(){
+        int[] num = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum1(num));
+    }
+
+    //直接暴力算法超时 所以使用二分法试试
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j <nums.length ; j++ ) {
+                for (int k = 0; k < nums.length; k++) {
+                    if(j != i && j!=k && i!=k && nums[i] + nums[j] == - nums[k]){
+                        List<Integer> list = Arrays.asList(nums[i],nums[j],nums[k]);
+                        list.sort(Integer::compareTo);
+                        lists.add(list);
+                    }
+                }
+            }
+        }
+
+        Set<List<Integer>> set = new HashSet<>();
+        set.addAll(lists);
+        List<List<Integer>> lists1 = new ArrayList<>();
+        lists1.addAll(set);
+
+        return lists1;
+    }
+
+    //使用二分法
+    public List<List<Integer>> threeSum1(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        int jNum;
+        int iNum;
+        int kNum;
+        for (int i = 0; i < nums.length; i++) {
+            int j = i+1;
+            int k = nums.length-1;
+            Arrays.sort(nums);
+            while (j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum > 0){
+                    k--;
+                }else if(sum < 0){
+                    j++;
+                }else {
+                    List<Integer> list = Arrays.asList(nums[i],nums[j],nums[k]);
+                    list.sort(Integer::compareTo);
+                    lists.add(list);
+
+                    jNum = nums[j];
+                    do {
+                        j++;
+                        if(j >= k){
+                            break;
+                        }
+                    }while (nums[j] ==jNum);
+                    kNum = nums[k];
+                    do {
+                        k--;
+                        if (j >= k){
+                            break;
+                        }
+                    }while (kNum == nums[k]);
+                }
+            }
+        }
+        Set<List<Integer>> set = new HashSet<>();
+        set.addAll(lists);
+        List<List<Integer>> lists1 = new ArrayList<>();
+        lists1.addAll(set);
+        return lists1;
+    }
+}
